5441e243a4706415451dc4bc8f701f70fa385f62
Ola Połaniecka <97olka@gmail.com>
2024-01-14 10:38:30 +0100

    Update index.js

--- goit-react-hw-01-components/src/index.js
+++ goit-react-hw-01-components/src/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// eslint-disable-next-line
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div class="profile">
+    <div class="description">
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+        alt="User avatar"
+        class="avatar"
+      />
+      <p class="name">Petra Marica</p>
+      <p class="tag">@pmarica</p>
+      <p class="location">Salvador, Brasil</p>
+    </div>
+
+    <ul class="stats">
+      <li>
+        <span class="label">Followers</span>
+        <span class="quantity">1000</span>
+      </li>
+      <li>
+        <span class="label">Views</span>
+        <span class="quantity">2000</span>
+      </li>
+      <li>
+        <span class="label">Likes</span>
+        <span class="quantity">3000</span>
+      </li>
+    </ul>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
