4aad9959f8239378e4bf049fa7a4d5d72ad53e59
Ola Połaniecka <97olka@gmail.com>
2024-01-14 11:01:30 +0100

    changes

--- goit-react-hw-01-components/src/index.js
+++ goit-react-hw-01-components/src/index.js
@@ -6,35 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div class="profile">
-    <div class="description">
-      <img
-        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-        alt="User avatar"
-        class="avatar"
-      />
-      <p class="name">Petra Marica</p>
-      <p class="tag">@pmarica</p>
-      <p class="location">Salvador, Brasil</p>
-    </div>
-
-    <ul class="stats">
-      <li>
-        <span class="label">Followers</span>
-        <span class="quantity">1000</span>
-      </li>
-      <li>
-        <span class="label">Views</span>
-        <span class="quantity">2000</span>
-      </li>
-      <li>
-        <span class="label">Likes</span>
-        <span class="quantity">3000</span>
-      </li>
-    </ul>
-  </div>
-);
+root.render(<div>Hello world!!</div>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
